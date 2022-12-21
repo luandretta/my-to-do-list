797764e7ac0ff84a4dbe0f066acd1787f7749d4a
Lucimeri Andretta <luandretta@hotmail.com>
2022-12-21 11:36:08 +0000

    docs: add  readme.md

--- README.md
+++ README.md
@@ -1,1 +1,147 @@
-My to do list - to organize and prioritize your tasks
+# My to do list
+
+My to do list is a websit to help you to organize and prioritize your tasks. You can add your tasks, then check it, if the task has been done, or edit or remove it.
+
+Those who are organized can plan their tasks and dedicate a better time for each activity, without wasting it. It is worth mentioning that generally those who lack planning tend to procrastinate during their day, which greatly hinders their activities and performance. 
+
+
+## Demo
+
+A live demo can be found [here](https://luandretta.github.io/my-to-do-list/)
+
+- - -
+
+# Contents
+# Contents
+* [Languages Used](#languages-used)
+* [User Experience](#user-experience-ux)
+* [Design](#design)
+  * [Features](#features)
+  * [Acessibility](#accessibility)
+  * [Color Scheme](#color-scheme)
+  * [Typography](#typography)
+  * [Icons](#icons)
+* [Deployment](#deployment)
+  * [Run locally](#run-locally)
+  * [Testing](#testing)
+* [Credits](#credits)
+
+- - - 
+
+# Languages Used
+* JavaScript
+* HTML5
+* CSS3
+- - - 
+
+# User Experience (UX)
+
+## The ideal users for this website is:
+* Users who want to have planning improves productivity.
+* Users who want to organize their tasks.
+
+## User stories:
+* As a new user, I expetc to easily to understand the main purpose of the site and navigate without complications.
+* I can create my to do list, either edit, or check or remove the tasks.
+* If I have many tasks, I expect to find a task easily so I can check it out.
+* I also want to select the done and not done tasks.
+
+- - -
+# Design
+
+
+## Features
+
+The website have only one page, the index page that contains a background image and a centralized main area.
+The first input is the task generator, where the user will add his or her tasks.
+Each added task will be added below the search and filter tools.
+Each task listed will have three buttons, to check, edit or remove.
+Clicking the **check button** will change the color of the task and give you a line about the task.
+By clicking on the **edit button** the user can edit the task and save it.
+By clicking the **remove button** the user will delete the task.
+
+Using the search tool the users can find the desired task to check, and using the filter tool the users can select either all tasks, or tasks done, or tasks that need to be done.
+
+The footer with Social Icon allows the users to get in touch with the creator of the page.
+
+## Accessibility
+The website is as accessible as possible:
+- Using semantic HTML
+- Ensuring that there is a sufficient colour contrast throughtout the site.
+- Using descriptive aria-label in the buttons
+- Providing information for screen readears where there are icons used and no text.
+- Confirming through lighthouse in devtools.
+
+## Color Scheme
+Blue tones were chosen to have harmony on the site, matching the background, which radiates peace. The color blue is linked to productivity and success, and also transmits tranquility; perfect for looking at your tasks without feeling under pressure.
+
+## Typography
+The fonts Ubuntu and Lora are imported from Google Fonts.
+
+## Icons
+The icons were from [font awesome](https://fontawesome.com/icons).
+
+- - -
+# Deployment
+This site is hosted using GitHub pages, deployed directly from the master branch. The deployed site will update automatically upon new commits to the master branch. In order for the site to deploy correctly on GitHub pages, the landing page must be named index.html.
+
+To deploy this page to GitHub Pages from its [GitHub repository](https://github.com/luandretta/night-clicks), the following steps were taken: 
+1. Login or Sign Up to GitHub.
+2. Open the project repository.
+3. From the menu items near the top of the page, select **Settings**.
+4. Click on "Pages" in the left hand navigation panel.
+5. Under "Source", choose which branch to deploy. This should be Main for newer repositories (older repositories may still use Master).
+6. Choose which folder to deploy from, usually "/root".
+7. Click "Save", then wait for it to be deployed. 
+It can take some time for the page to be fully deployed.
+8. Your URL will be displayed above "Source"
+
+### Run locally
+**Fork**
+1. Login or Sign Up to GitHub.
+2. Open the project [repository](https://github.com/luandretta/night-clicks).
+3. Click the Fork button in the top right corner.
+
+**Clone**
+1. Login or Sign Up to GitHub.
+2. Open the project [repository](https://github.com/luandretta/night-clicks).
+3. Click on the code button, select whether you would like to clone with HTTPS, SSH or GitHub CLI and copy the link shown.
+4. Open the terminal in the code editor of your choice and change the current working directory to the location you want to use for the cloned directory.
+5. Type 'git clone' into the terminal and then paste the link you copied in step 3. Press enter.
+
+# Testing 
+The different aspects of the site work as intended and have an expected outcome providing an easy and straightforward way for the users to achieve their goals.
+Chrome development tools were used during the development of the site to identify and resolve any issues as it progressed.
+The performance was tested manual and automated testing.
+
+## Functionality
+1. W3C Markup Validator, W3C CSS Validator Services and Jshint were used to validate this project to ensure that there were no syntax errors.
+
+2. Lighthouse within the Chrome Developer Tools are used to test performance, accessibility, best practices and SEO of the My to do list Website.
+
+3. Manual as an user.
+
+
+## Compatibility
+The website displays correctly across different browsers and screen sizes.
+
+It was checked on Chrome, Firefox, Safari and Edge.
+
+## Responsiveness
+Chrome developer tool have been used to check the responsivness. The application was tested on very small screens e.g. iPhone 5(320px wide) and very large screens e.g. 5k iMac Pro(5120x2880px).
+
+Media queries are used for more responsiveness.
+
+## Solved bugs
+* When clicking on the icon button as content, it did not activate the event, only if I clicked on the area outside the icon. Rewrote the function of each button using e.currentTarget to fix it.
+
+# Credits
+## Code
+The code was based on a tutorial from [w3schools](https://www.w3schools.com/howto/howto_js_todolist.asp) with some changes and additions.
+
+##
+* My family for their patiences as I disappeared to code during the Christmas time.
+
+* My husband for all the support.
+
+* My Mentor Brian Macharia for continuous helpful feedback.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,108 +1,133 @@
-//Wait for the DOM to finish loading before running the quiz
-
+//Wait for the DOM to finish loading before users interaction
 let idCount = 0;
 document.addEventListener("DOMContentLoaded", function () {
 
 
-    // declaration of the variables
-    const todoForm = document.querySelector("#todo-form");
-    const todoInput = document.querySelector("#todo-input");
-    const editForm = document.querySelector("#edit-form");
-    const editInput = document.querySelector("#edit-input");
-    const cancelEditBtn = document.querySelector("#cancel-edit-btn");
-    const todoList = document.querySelector("#todo-list");
+// declaration of the variables
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+const editForm = document.querySelector("#edit-form");
+const editInput = document.querySelector("#edit-input");
+const cancelEditBtn = document.querySelector("#cancel-edit-btn");
+const todoList = document.querySelector("#todo-list");
+const finishTodoButton = document.querySelector("finish-todo");
+const editTodoButton = document.querySelector("edit-todo");
+const removeTodoButton = document.querySelector("remove-todo");
 
-    /**
-     * prevent that information wont be sent to back end
-     * and save todo value
-     */
-    todoForm.addEventListener("submit", (e) => {
-        const inputValue = todoInput.value;
-        e.preventDefault();
+/**
+ * prevent that information wont be sent to back end
+ * and save todo value
+ */
+todoForm.addEventListener("submit", (e) => {
+    const inputValue = todoInput.value.trim();
+    e.preventDefault();
 
-        if (inputValue) {
+    if (inputValue) {
 
-            saveTodo(todoList, todoInput);
+        saveTodo(todoList, todoInput);
 
-        }
-    });
+    }
+});
 
-    /**
-     * event listeners to the buttons
-     */
+/**
+ * event listeners to the buttons
+ */
+
+//finish button 
+finishTodoButton.addEventListener("click", (e) => {
+    const targetEl = e.currentTarget;
+    const parentEl = targetEl.closest("div");
+    parentEl.classList.toggle("done");
+})
+
+// remove task button
+removeTodoButton.addEventListener("click", (e) => {
+    const targetEl = e.currentTarget;
+    const parentEl = targetEl.closest("div");
+    parentEl.classList.remove;
+})
+
+//edit button
+editTodoButton.addEventListener("click", (e) => {
+    const targetEl = e.currentTarget;
+    const parentEl = targetEl.closest("div");
+    parentEl.classList.toggle("done");
+})
+
+
+
+/*
+document.addEventListener("click", (e) => {
+    const targetEl = e.target;
+    console.log(targetEl);
     
-    //document.addEventListener("click", (e) => {
-    document.addEventListener("click", (e) => {
-        const targetEl = e.target;
-        console.log(targetEl);
-        
-        const parentEl = targetEl.closest("div");
-        let todoTask;
+    const parentEl = targetEl.closest("div");
+    let todoTask;
 
-        if (parentEl && parentEl.querySelector("h3")) {
-            todoTask = parentEl.querySelector("h3").innerText;
-        }
+    if (parentEl && parentEl.querySelector("h3")) {
+        todoTask = parentEl.querySelector("h3").innerText;
+    }
 
-        if (targetEl.classList.contains("finish-todo") || targetEl.parentNode.classList.contains("finish-todo")) {
-            console.log(targetEl.classList);
-            parentEl.classList.toggle("done");
-        }
+    if (targetEl.classList.contains("finish-todo") || targetEl.parentNode.classList.contains("finish-todo")) {
+        console.log(targetEl.classList);
+        parentEl.classList.toggle("done");
+    }
 
-        if (targetEl.classList.contains("remove-todo") || targetEl.parentNode.classList.contains("remove-todo")) {
-            parentEl.remove();
-        }
+    if (targetEl.classList.contains("remove-todo") || targetEl.parentNode.classList.contains("remove-todo")) {
+        parentEl.remove();
+    } */
 
-        if (targetEl.classList.contains("edit-todo") || targetEl.parentNode.classList.contains("edit-todo")) {
-                console.log(todoTask);
-                console.log("-----------");
-                let id = targetEl.id;
-                let text = document.getElementById("h3" + id).innerHTML;
-                const h3 = document.getElementById("h3" + id);
-                console.log(h3.value);
-                console.log(text);
-                const editInputText = document.createElement("input");
-                editInputText.setAttribute("type", "text");
-                editInputText.name = "editInput" + id;
-                console.log(editInputText.name);
-                editInputText.value = todoTask;
-                h3.replaceWith(editInputText);
-                const doneBtn = document.getElementsByName("done" + id)[0];
-                doneBtn.style.display = 'none';
-                const editBtn = document.getElementsByName("edit" + id)[0];
-                editBtn.style.display = 'none';
-                const deleteBtn = document.getElementsByName("delete" + id)[0];
-                deleteBtn.style.display = 'none';
+if (targetEl.classList.contains("edit-todo") || targetEl.parentNode.classList.contains("edit-todo")) {
+    console.log(todoTask);
+    console.log("-----------");
+    let id = targetEl.id;
+    let text = document.getElementById("h3" + id).innerHTML;
+    const h3 = document.getElementById("h3" + id);
+    console.log(h3.value);
+    console.log(text);
+    const editInputText = document.createElement("input");
+    editInputText.setAttribute("type", "text");
+    editInputText.name = "editInput" + id;
+    console.log(editInputText.name);
+    editInputText.value = todoTask;
+    h3.replaceWith(editInputText);
+    const doneBtn = document.getElementsByName("done" + id)[0];
+    doneBtn.style.display = 'none';
+    const editBtn = document.getElementsByName("edit" + id)[0];
+    editBtn.style.display = 'none';
+    const deleteBtn = document.getElementsByName("delete" + id)[0];
+    deleteBtn.style.display = 'none';
 
-                const saveButton = document.createElement("button");
-                saveButton.classList.add("edit-save");
-                saveButton.id = id;
-                saveButton.name = "save" + id;
-                saveButton.innerHTML = '<i class="fa-solid fa-save" id="'+ id +'"></i>';
-                editInputText.parentNode.insertBefore(saveButton, editInputText.nextSibling);
-                editInputText.focus();
-            }
-            if (targetEl.classList.contains("edit-save") || targetEl.parentNode.classList.contains("edit-save")){
-                let id = targetEl.id;
-                console.log(id);
-                const editInputText = document.getElementsByName("editInput" + id)[0];
-                text_updated = editInputText.value;
-                editInputText.style.display = "none";
-                const h3 = document.createElement("h3");
-                h3.id = "h3" + id;
-                h3.innerHTML = text_updated;
-                editInputText.replaceWith(h3);
-                const doneBtn = document.getElementsByName("done" + id)[0];
-                doneBtn.style.display = "flex";
-                const editBtn = document.getElementsByName("edit" + id)[0];
-                editBtn.style.display = "flex";
-                const deleteBtn = document.getElementsByName("delete" + id)[0];
-                deleteBtn.style.display = "flex";
-                const saveBtn = document.getElementsByName("save" + id)[0];
-                saveBtn.style.display = "none";
-                
-            //toggleForms();
-        }
-    });
+    const saveButton = document.createElement("button");
+    saveButton.classList.add("edit-save");
+    saveButton.id = id;
+    saveButton.name = "save" + id;
+    saveButton.innerHTML = '<i class="fa-solid fa-save" id="' + id + '"></i>';
+    editInputText.parentNode.insertBefore(saveButton, editInputText.nextSibling);
+    editInputText.focus();
+}
+if (targetEl.classList.contains("edit-save") || targetEl.parentNode.classList.contains("edit-save")) {
+    let id = targetEl.id;
+    console.log(id);
+    const editInputText = document.getElementsByName("editInput" + id)[0];
+    text_updated = editInputText.value;
+    editInputText.style.display = "none";
+    const h3 = document.createElement("h3");
+    h3.id = "h3" + id;
+    h3.innerHTML = text_updated;
+    editInputText.replaceWith(h3);
+    const doneBtn = document.getElementsByName("done" + id)[0];
+    doneBtn.style.display = "flex";
+    const editBtn = document.getElementsByName("edit" + id)[0];
+    editBtn.style.display = "flex";
+    const deleteBtn = document.getElementsByName("delete" + id)[0];
+    deleteBtn.style.display = "flex";
+    const saveBtn = document.getElementsByName("save" + id)[0];
+    saveBtn.style.display = "none";
+
+    //toggleForms();
+}
+});
 
 });
 
@@ -128,21 +153,21 @@ function saveTodo(todo_list, todo_input) {
     doneBtn.classList.add("finish-todo");
     doneBtn.id = idCount;
     doneBtn.name = "done" + idCount;
-    doneBtn.innerHTML = '<i class="fa-solid fa-check" id="'+ idCount +'"></i>';
+    doneBtn.innerHTML = '<i class="fa-solid fa-check" id="' + idCount + '"></i>';
     todo.appendChild(doneBtn);
 
     const editBtn = document.createElement("button");
     editBtn.classList.add("edit-todo");
     editBtn.id = idCount;
     editBtn.name = "edit" + idCount;
-    editBtn.innerHTML = '<i class="fa-solid fa-pen" id="'+ idCount +'"></i>';
+    editBtn.innerHTML = '<i class="fa-solid fa-pen" id="' + idCount + '"></i>';
     todo.appendChild(editBtn);
 
     const deleteBtn = document.createElement("button");
     deleteBtn.classList.add("remove-todo");
     deleteBtn.id = idCount;
     deleteBtn.name = "delete" + idCount;
-    deleteBtn.innerHTML = '<i class="fa-solid fa-xmark" id="'+ idCount +'"></i>';
+    deleteBtn.innerHTML = '<i class="fa-solid fa-xmark" id="' + idCount + '"></i>';
     todo.appendChild(deleteBtn);
 
     todo_list.append(todo);
@@ -150,5 +175,3 @@ function saveTodo(todo_list, todo_input) {
     todo_input.focus();
     idCount++;
 };
-
-// events
